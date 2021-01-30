4a0fae180e66f935ad389a38ebd6220681765373
Thomas Braccia <bracciata@gmail.com>
2021-01-30 14:02:14 -0600

    Revert "Fixed the iframe of the agent."
    This reverts commit bf8931df1dd327097adae896d77082ebb326c13b.

--- teddy/src/App.js
+++ teddy/src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
-import { Row, Col } from 'react-bootstrap';
+import React from 'react';
+import { Container, Row, Col, Navbar } from 'react-bootstrap';
 
 function App() {
   return (
-    <Row>
-      <Col>
-        <iframe
-          allow="microphone;"
-          width="350"
-          height="430"
-          src="https://console.dialogflow.com/api-client/demo/embedded/baacb116-5167-4c03-b8e3-ee85a43bb82d">
-        </iframe>
-      </Col>
-    </Row>
+    <Container fluid>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="#home">
+          Teddy
+        </Navbar.Brand>
+      </Navbar>
+      <img
+            alt="Teddy Logo"
+            src="/logo.svg"
+            width="50"
+            height="50"
+            className="d-inline-block align-top"
+          />
+      <Row>
+        <Col>
+          <iframe
+            allow="microphone;"
+            width="350"
+            height="430"
+            border="1px solid black"
+            src="https://console.dialogflow.com/api-client/demo/embedded/baacb116-5167-4c03-b8e3-ee85a43bb82d%22%3E">
+          </iframe>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
