38e5f05c372e99a0bb63b63b086599ece22eab89
Thomas Braccia <bracciata@gmail.com>
2021-01-30 17:27:23 -0600

    Added kommunicate bot.

--- teddy/src/App.js
+++ teddy/src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
-import React from 'react';
+import React,{useEffect} from 'react';
 import { Container, Row, Col, Navbar } from 'react-bootstrap';
 
 function App() {
+  useEffect(() => {
+    (function(d, m){
+      var kommunicateSettings = 
+          {"appId":"2a31e1417de82755606bcd6c32ac2d4ad","popupWidget":true,"automaticChatOpenOnNavigation":true};
+      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+      window.kommunicate = m; m._globals = kommunicateSettings;
+  })(document, window.kommunicate || {});
+  }, []);
   return (
     <Container fluid className="no-padding">
       <Navbar className="navbar">
@@ -38,6 +48,7 @@ function App() {
         </div>
       </div>
     </Container>
+    
   );
 }
 
