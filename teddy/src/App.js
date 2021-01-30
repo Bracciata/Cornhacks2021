af616e3835ca0b72243a8608bb3ebaa4687ea0ca
Thomas Braccia <bracciata@gmail.com>
2021-01-30 14:03:09 -0600

    Fixed issues in last commit caused by overwriting Emma's navbar.

--- teddy/src/App.js
+++ teddy/src/App.js
@@ -11,20 +11,19 @@ function App() {
         </Navbar.Brand>
       </Navbar>
       <img
-            alt="Teddy Logo"
-            src="/logo.svg"
-            width="50"
-            height="50"
-            className="d-inline-block align-top"
-          />
+        alt="Teddy Logo"
+        src="/logo.svg"
+        width="50"
+        height="50"
+        className="d-inline-block align-top"
+      />
       <Row>
         <Col>
           <iframe
             allow="microphone;"
             width="350"
             height="430"
-            border="1px solid black"
-            src="https://console.dialogflow.com/api-client/demo/embedded/baacb116-5167-4c03-b8e3-ee85a43bb82d%22%3E">
+            src="https://console.dialogflow.com/api-client/demo/embedded/baacb116-5167-4c03-b8e3-ee85a43bb82d">
           </iframe>
         </Col>
       </Row>
