d18c3ffeec63f43151d5a8e9f1f6c362edd2b524
Emma Hubka <34105895+emma-hubka@users.noreply.github.com>
2021-01-30 12:00:31 -0600

    Added bootstrap to the app.

--- teddy/src/App.js
+++ teddy/src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Row>
+      <Col>
+        <iframe
+          allow="microphone;"
+          width="350"
+          height="430"
+          border="1px solid black"
+          src="https://console.dialogflow.com/api-client/demo/embedded/baacb116-5167-4c03-b8e3-ee85a43bb82d%22%3E">
+        </iframe>
+      </Col>
+    </Row>
   );
 }
 
