4705a0a2af8d0c8f800550426a113b6e09db447b
Danny Tran <danny.tran2018@gmail.com>
2021-01-30 23:27:21 -0600

    Added functionality to the navigation bar.

--- teddy/src/App.js
+++ teddy/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React from 'react';
 import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
+import {BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <Container fluid className="no-padding">
+      <Router>
       <Navbar className="navbar">
-        <Navbar.Brand className="full-width" href="#home">
+        <Navbar.Brand className="full-width" href="/">
           <div className="logo">
             <img
                 alt="Teddy Logo"
@@ -18,22 +20,23 @@ function App() {
             <div className="logo-text">Teddy</div>
           </div>
         </Navbar.Brand>
-
-        <Nav className="ml-auto">
-          <Nav.Link className="nav-option" href="#home">Resources</Nav.Link>
-          <Nav.Link className="nav-option" href="#features">About</Nav.Link>
-        </Nav>
-            
-
+          <Nav className="ml-auto">
+            <Link to="/about" className="nav-option">About</Link>
+            <Link to="/resources" className="nav-option">Resources</Link>
+          </Nav>
       </Navbar>
-
-      <Row className="no-margin">
-        <Col xs={{ span: 8, offset: 2 }}>
-          <div className="iframe-container">
-            <iframe width="100%" height="100%" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/baacb116-5167-4c03-b8e3-ee85a43bb82d"></iframe>
-          </div>
-        </Col>
-      </Row>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/about">
+            <About/>
+          </Route>
+          <Route path="/resources">
+            <Resources/>
+          </Route>
+        </Switch>
+      </Router>
     
       <div className="footer">
         <div className="footer-header">
@@ -45,6 +48,33 @@ function App() {
         </div>
       </div>
     </Container>
+  );
+}
+
+function Home(){
+  return(
+    <Row className="no-margin">
+        <Col xs={{ span: 8, offset: 2 }}>
+          <div className="iframe-container">
+            <iframe width="100%" height="100%" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/baacb116-5167-4c03-b8e3-ee85a43bb82d"></iframe>
+          </div>
+        </Col>
+      </Row>
+  );
+}
+function Resources() {
+  return (
+    <div>
+      <h2>TEsts</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>TEstsdsdsdsdsds</h2>
+    </div>
   );
 }
 
